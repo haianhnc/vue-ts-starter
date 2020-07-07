2fc83bf96eef718943f43d242c60aec0807129a4
Do Minh Hai <haianhnc@mirabo.com.vn>
2020-07-07 15:34:12 +0700

    [Add] Layout and Logic

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+// import '@fortawesome/fontawesome-free/js/all.js';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/bulma/css/bulma.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
